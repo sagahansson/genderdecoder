c5a27cfce31ecb43385afb4f80fefe806e314540
Saga Hansson <gushansad@GU.GU.SE@mltgpu.flov.gu.se>
2021-02-05 17:38:13 +0100

    added sys path

--- genderdecoder/__init__.py
+++ genderdecoder/__init__.py
@@ -1,3 +1,8 @@
+import sys
+import os
+current_dir = os.getcwd()
+sys.path.append(current_dir + "/genderdecoder/genderdecoder")
+
 from assess import assess
 from wordlists import feminine_coded_words
 from wordlists import masculine_coded_words
