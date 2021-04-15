db18f51c8a3708aac9f4ba5d71e4c8f78df33257
Konstantinos <gusmavko@student.gu.se>
2021-04-15 09:09:13 +0200

    change three label classification on algorithm we modified

--- genderdecoder/__init__.py
+++ genderdecoder/__init__.py
@@ -3,6 +3,6 @@ import os
 current_dir = os.getcwd()
 sys.path.append(current_dir + "/genderdecoder/genderdecoder")
 
-from assess import assess
+from assess import assess, assess_v2, assess_v3
 from wordlists import feminine_coded_words
 from wordlists import masculine_coded_words
